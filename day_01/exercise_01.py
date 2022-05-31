5d676f399cf675d664ee17cc0b38303faefc9e5a
Sam Hakem <samhakem@egmail.com>
2022-05-31 18:19:52 +0200

    first commit and structure setup

---
+++ day_01/exercise_01.py
@@ -0,0 +1,5 @@
+#Write your code below this line 👇
+# These are the notes from the previous lesson.
+print('Day 1 - Python Print Function')
+print('The function is declared like this:')
+print("print('what to print')")
---
+++ day_01/exercise_03.py
@@ -0,0 +1,4 @@
+#Write your code below this line 👇
+name = input('What is your name?')
+length = len(name)
+print(length)
