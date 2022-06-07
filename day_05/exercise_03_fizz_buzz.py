b6aa5549079e24681b78ca4eefb1e49ab94285ae
Sam Hakem <samhakem@egmail.com>
2022-06-07 11:25:26 +0200

    first commit and structure setup

--- day_05/exercise_03_fizz_buzz.py
+++ day_05/exercise_03_fizz_buzz.py
@@ -1,30 +1,42 @@
 # Fizz Buzz
 
-class Solution:
-    def fizzBuzz(self, n):
-        """
-        :type n: int
-        :rtype: List[str]
-        """
-        # ans list
-        ans = []
+# My inelegant naive solution:
+for num in range(1, 100):
+    if num % 3 == 0 and num % 5 == 0:
+        print('Fizzbuzz')
+    elif num % 3 == 0:
+        print('Fizz')
+    elif num % 5 == 0:
+        print('Buzz')
+    else:
+        print(num)
 
-        for num in range(1,n+1):
-
-            divisible_by_3 = (num % 3 == 0)
-            divisible_by_5 = (num % 5 == 0)
-
-            if divisible_by_3 and divisible_by_5:
-                # Divides by both 3 and 5, add FizzBuzz
-                ans.append("FizzBuzz")
-            elif divisible_by_3:
-                # Divides by 3, add Fizz
-                ans.append("Fizz")
-            elif divisible_by_5:
-                # Divides by 5, add Buzz
-                ans.append("Buzz")
-            else:
-                # Not divisible by 3 or 5, add the number
-                ans.append(str(num))
-
-        return ans
+# A beautiful class solution for me to study.
+# class Solution:
+#     def fizzBuzz(self, n):
+#         """
+#         :type n: int
+#         :rtype: List[str]
+#         """
+#         # ans list
+#         ans = []
+#
+#         for num in range(1,n+1):
+#
+#             divisible_by_3 = (num % 3 == 0)
+#             divisible_by_5 = (num % 5 == 0)
+#
+#             if divisible_by_3 and divisible_by_5:
+#                 # Divides by both 3 and 5, add FizzBuzz
+#                 ans.append("FizzBuzz")
+#             elif divisible_by_3:
+#                 # Divides by 3, add Fizz
+#                 ans.append("Fizz")
+#             elif divisible_by_5:
+#                 # Divides by 5, add Buzz
+#                 ans.append("Buzz")
+#             else:
+#                 # Not divisible by 3 or 5, add the number
+#                 ans.append(str(num))
+#
+#         return ans
