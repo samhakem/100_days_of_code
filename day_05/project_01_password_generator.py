4605607878e286b3d935c91b10fef4b7e2617d57
Sam Hakem <samhakem@egmail.com>
2022-06-07 15:49:31 +0200

    first commit and structure setup

--- day_05/project_01_password_generator.py
+++ day_05/project_01_password_generator.py
@@ -1,5 +1,5 @@
 # Password Generator Project
-# import random
+
 import random
 
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
