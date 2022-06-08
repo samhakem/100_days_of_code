8f4577d9a7ede8ab92c9b6665c3a96b467f196e4
Sam Hakem <samhakem@egmail.com>
2022-06-08 12:06:24 +0200

    first commit and structure setup

---
+++ day_06/exercise_01_functions.py
@@ -0,0 +1,15 @@
+# Defining and calling functions
+
+# Built in function
+print('Hello')
+num_char = len('Hello')
+print(num_char)
+
+# Define function with def keyword followed by a parantheses and line ending colon
+
+def my_function():
+    print('Hello')
+    print('Bye')
+
+# Call a function by calling the function name followed by parentheses - where any params might go
+my_function()
