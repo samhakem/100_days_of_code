3c8600a2df732a672f3216705ed154f2af285950
Sam Hakem <samhakem@egmail.com>
2022-06-09 13:56:58 +0200

    first commit and structure setup

--- day_06/exercise_03_indentation.py
+++ day_06/exercise_03_indentation.py
@@ -2,6 +2,11 @@
 
 def my_function():
 ....if sky == '4_spaces':
-........print('8_spaces')
-....elif sky == (4_spaces):
+........if night == '8_spaces':
+............print('12_spaces')
+....elif sky == '4_spaces':
 print('outside the code block')
+
+# Spaces are preferred indentation method
+
+    # Use four separate hits of the space bar :S
