62bfa416c00f1795e3b6f0eb776cd59ee0101574
Sam Hakem <samhakem@egmail.com>
2022-06-09 14:18:13 +0200

    first commit and structure setup

---
+++ day_06/exercise_04_while_loops.py
@@ -0,0 +1,22 @@
+# While loop
+
+while something_is_true:
+    # Do something repeatedly
+
+# https://reeborg.ca/
+
+def jump_1():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+number_of_hurdles = 4
+while number_of_hurdles > 0:
+    jump_1()
+    number_of_hurdles -= 1
+    print(number_of_hurdles)
