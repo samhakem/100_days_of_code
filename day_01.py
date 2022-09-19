f41ac23d064a59c41932c466bbadcfbee64169eb
sam <samhakem@gmail.com>
2022-09-19 17:46:53 +0200

    Day 01 - commit

---
+++ day_01.py
@@ -0,0 +1,44 @@
+# Day 01
+# Print statements
+
+# Prints a string into the console. print("Hello World")
+
+print('Im a string')  # Print something in the parentheses using quotation marks to delineate a string
+
+# Error handling
+
+#print('Im an error!)  # Syntax highlighting error using an underline
+
+# print('Im an error)
+#           ^
+# SyntaxError: unterminated string literal (detected at line 10)
+
+# Exercise 01
+# These are the notes from the previous lesson.
+print('Day 1 - Python Print Function')
+print('The function is declared like this:')
+print("print('what to print')")
+
+# Print to a new line \n
+print('This is how\n')
+print('You print to a new line')
+
+# Multi print lines with one statement, no spaces between the \n is important
+print('A line\nand another line\nand yet another.')
+
+# String concatenation
+print('4' + '2')
+
+string_1 = '4'
+string_2 = '2'
+
+print(string_1 + string_2)
+
+# With added spaces
+print('An example of a' + ' ' + 'space.')
+
+# Indentation error
+
+ print('Im an indentation error')
+# print('Im an indentation error')
+# IndentationError: unexpected indent
